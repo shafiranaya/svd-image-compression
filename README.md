1bfbe0fa50d67d9420b4773ae2584bda76222b1b
shafiranaya <shfrnaya@gmail.com>
2021-06-28 23:03:39 +0700

    edit readme dan lain lain

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ Tugas Seleksi Ca-IRK 2019<br>
 Spek: https://github.com/williammfu/svd-image-compression
 
 ## TODOLIST (nanti didelete)
-- [ ] bikin format tingkat kompresi (-> kayak brp persen gitu nnt diconvert ke limit - gunain shape
-- [ ] run time program
-- [ ] persentase ukuran memori gambar yang dikompresi thd gambar original
-- [ ] readme
+- [ ] bikin format tingkat kompresi (-> kayak brp persen gitu nnt diconvert ke limit - gunain shape 
+- [x] run time program
+- [ ] persentase ukuran memori gambar yang telah dikompresi thd gambar original -> ternyata masih anehh hasil kompresi nya lebih besar :< pdhl kualitas nya jelek
+- [] readme
 - [ ] huffman coding
-- [ ] algoritma svd sendiri
+- [x] algoritma svd sendiri (belom perfect)
 ## Cara penggunaan program
 
 
@@ -24,10 +24,12 @@ https://www.d.umn.edu/~mhampton/m4326svd_example.pdf
 
 http://www.math.utah.edu/~goller/F15_M2270/BradyMathews_SVDImage.pdf
 
-https://www.lagrange.edu/academics/undergraduate/undergraduate-research/citations/18-Citations2020.Compton.pdf
+https://www.lagrange.edu/academics/undergraduate/undergraduate-research/citations/18-Citations2020.Compton.pdf -> referensi ini adalah referensi utama. Penjelasannya sangat lengkap dan step by step sehingga mudah diimplementasikan dalam membuat algoritma SVD from scratch.
 
-https://www.youtube.com/watch?v=SU851ljMIZ8
+https://www.youtube.com/watch?v=SU851ljMIZ8 -> berguna :)
+https://www.youtube.com/watch?v=2lXaCh-dnRk cukup memberi gambaran:)
 
 ## Framework dan Library
-- Numpy
-- Opencv2
+- Numpy, untuk melakukan operasi matriks, seperti matmul (matrix multiplication) serta melakukan operasi linalg (algoritma svd, mencari eigenvalue, eigenvector), dll
+- Opencv2, untuk membaca gambar lalu mereturn gambar tersebut dalam bentuk matriks
+- Datetime, untuk menghitung waktu eksekusi program
--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import numpy as np
 import cv2
 from svd import svd
+import datetime
+import os
+ 
 
 def read_image(img_name):
     path = 'in/' + img_name
     img = cv2.imread(path)
-    return img
+    file_size = os.path.getsize(path)
+    return img, file_size
 
 def compress_channel(img, limit):
-    # U, S, Vt = np.linalg.svd(img)
-    U, S, Vt = svd(img)
+    U, S, Vt = np.linalg.svd(img)
+    # U, S, Vt = svd(img)
     U_new = U[:,0:limit]
-    # S_new = np.diag(S)[0:limit,0:limit]
-    S_new = S[0:limit,0:limit]
+    S_new = np.diag(S)[0:limit,0:limit]
+    # S_new = S[0:limit,0:limit]
     Vt_new = Vt[0:limit, : ]
     img_new = np.matmul(np.matmul(U_new,S_new),Vt_new)
     return img_new
@@ -35,30 +39,39 @@ def compress_rgb(img, limit):
 def write_image(img, img_name, limit):
     path = 'out/' + img_name + '_' + str(limit) + '.png'
     cv2.imwrite(path, img)
+    file_size = os.path.getsize(path)
+    return file_size
 
 # MAIN PROGRAM - TESTING
-n = 321
+n = 111
 # n gaboleh lebih besar dari shape
-momo = read_image('momo.jpeg')
+start = datetime.datetime.now()
+file_name = 'momo_gray.png'
+momo = read_image(file_name)[0]
+file_size_awal = read_image(file_name)[1]
 print(momo.shape)
-momo_compressed = compress_rgb(momo, n)
-write_image(momo_compressed,'momo_compressed_grayscale', n)
-print("SELESAI")
+momo_compressed = compress_grayscale(momo, n)
+file_size_akhir = write_image(momo_compressed,'momo_gray_compressed', n)
+end = datetime.datetime.now()
+print(momo_compressed.shape)
+print("Selesai dalam waktu",(end-start),"seconds.")
+print("File size awal:", file_size_awal, "bytes")
+print("File size akhir:", file_size_akhir, "bytes")
 
-# TESTING BENER APA ENGGA
-print("---TESTING---")
-A = cv2.cvtColor(momo, cv2.COLOR_BGR2GRAY)
+# # TESTING BENER APA ENGGA
+# print("---TESTING---")
+# A = cv2.cvtColor(momo, cv2.COLOR_BGR2GRAY)
 
-U, Sigma, Vt = svd(A)
-print("A: \n",A)
-A_test = np.matmul(np.matmul(U,Sigma), Vt)
-print("A_test: \n",A_test)
-print("U: \n",U)
-print("Sigma: \n",Sigma)
-print("Vt: \n",Vt)
+# U, Sigma, Vt = svd(A)
+# print("A: \n",A)
+# A_test = np.matmul(np.matmul(U,Sigma), Vt)
+# print("A_test: \n",A_test)
+# print("U: \n",U)
+# print("Sigma: \n",Sigma)
+# print("Vt: \n",Vt)
 
-# BANDINGIN SAMA HASIL LIBRARY
-U_lib, S_lib, Vt_lib = np.linalg.svd(A)
-print("U_lib: \n",U_lib)
-print("S_lib: \n",S_lib)
-print("Vt_lib: \n",Vt_lib)
+# # BANDINGIN SAMA HASIL LIBRARY
+# U_lib, S_lib, Vt_lib = np.linalg.svd(A)
+# print("U_lib: \n",U_lib)
+# print("S_lib: \n",S_lib)
+# print("Vt_lib: \n",Vt_lib)
