2661d4914fe244b2a96010c8d48181b0c5f74316
shafiranaya <shfrnaya@gmail.com>
2021-06-28 23:24:24 +0700

    aneh

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import cv2
 from svd import svd
 import datetime
 import os
- 
 
 def read_image(img_name):
     path = 'in/' + img_name
@@ -12,6 +11,7 @@ def read_image(img_name):
     return img, file_size
 
 def compress_channel(img, limit):
+    n = len(img)
     U, S, Vt = np.linalg.svd(img)
     # U, S, Vt = svd(img)
     U_new = U[:,0:limit]
@@ -19,7 +19,15 @@ def compress_channel(img, limit):
     # S_new = S[0:limit,0:limit]
     Vt_new = Vt[0:limit, : ]
     img_new = np.matmul(np.matmul(U_new,S_new),Vt_new)
-    return img_new
+    imgs = []
+    for i in range(n):
+        imgs.append(S[i]*np.outer(U[:,i],Vt[i]))
+    combined_imgs = []
+    for i in range(n):
+        img = sum(imgs[:i+1])
+        combined_imgs.append(img)
+    combined_imgs = combined_imgs[limit]
+    return img_new, combined_imgs
 
 def compress_grayscale(img, limit):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -37,21 +45,25 @@ def compress_rgb(img, limit):
     return compressed_rgb
 
 def write_image(img, img_name, limit):
-    path = 'out/' + img_name + '_' + str(limit) + '.png'
+    path = 'out/' + img_name + '_' + str(limit) + '.jpeg'
     cv2.imwrite(path, img)
+    # print("berhasil write")
     file_size = os.path.getsize(path)
     return file_size
 
 # MAIN PROGRAM - TESTING
-n = 111
-# n gaboleh lebih besar dari shape
+lim = 111
+# lim gaboleh lebih besar dari shape
 start = datetime.datetime.now()
-file_name = 'momo_gray.png'
+file_name = 'momo.jpg'
 momo = read_image(file_name)[0]
 file_size_awal = read_image(file_name)[1]
 print(momo.shape)
-momo_compressed = compress_grayscale(momo, n)
-file_size_akhir = write_image(momo_compressed,'momo_gray_compressed', n)
+# momo_compressed = compress_grayscale(momo, lim)[0]
+momo_compressed = np.array(compress_grayscale(momo, lim)[1])
+file_size_akhir = write_image(momo_compressed,'momo_jpg_grayscale_compressed', lim)
+
+write_image(momo_compressed,'momo_jpg_grayscale_compressed', lim)
 end = datetime.datetime.now()
 print(momo_compressed.shape)
 print("Selesai dalam waktu",(end-start),"seconds.")
